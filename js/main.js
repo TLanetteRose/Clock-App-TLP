0fb190bd93be6f8dc76c3d30067bc4ae54296e5f
T. Lanette <TLPollard4516@gmail.com>
2021-03-23 20:52:08 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Elements change displays/click btn
+let appContainer = document.getElementsByClassName("app__container");
+let footerContainer = document. getElementsByClassName("footer__container");
+let displayBtn = document.getElementById("btn-click");
+let btnLabel = document.getElementsByClassName("btn-label");
+let quote = document.getElementsByClassName("quote");
+let quoteCaption = document.getElementsByClassName("quote__caption");
+let refreshIcon = document.getElementsByClassName("refresh-icon");
+let quoteContainer = document.getElementsByClassName("quote__container");
