5b00d20bc3bf946ffe4885272c8146cdf9289545
T. Lanette <TLPollard4516@gmail.com>
2021-03-23 21:15:56 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -8,11 +8,19 @@ let quoteCaption = document.getElementsByClassName("quote__caption");
 let refreshIcon = document.getElementsByClassName("refresh-icon");
 let quoteContainer = document.getElementsByClassName("quote__container");
 
-// Change time of day
+// Change greeting
 let clockGreeting = document.getElementsByClassName("clock__greeting-container");
 let greeting = document.getElementsByClassName("clock-greeting");
 
 // Image for background changes in app
 let backgroundImg = document.getElementById("app");
+
+// Change clock icon
 let dayIcon = document.getElementsByClassName("day-icon");
 let nightIcon = document.getElementsByClassName("night-icon");
+
+// Display time
+let clock = document.getElementById("clock");
+let timeZone = document.getElementsByClassName("time-zone");
+
+// Display location
