62f12b2fbcb46bebbcb97dd9c993f90e32ccaf80
T. Lanette <TLPollard4516@gmail.com>
2021-03-27 12:39:44 -0400

    Update
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -6,3 +6,32 @@ const details = document.getElementsByClassName('.description');
 const period = document.getElementsByClassName('.period'); 
 const expand = document.getElementsByClassName('.expand');
 
+
+
+// Get current day and time
+function getTime() {
+let currentTime = new Date();
+let hour = currentTime.getHours();
+let minute = currentTime.getMinutes();
+
+// Setting greeting and time of day
+let greeting = '';
+if (hour >= 5 && hour <= 11){
+    greeting = 'morning';
+} else if (hour >= 12 && hour <= 17) {
+    greeting = 'afternoon';
+} else {
+    greeting = 'evening';
+}
+    document.querySelector('current__greeting').textContent = `good ${greeting}`
+
+    // Background and icon
+    if (hour >= 5 && hour <= 17) {
+        background.classList.add('day');
+        icon.src = '../assets/desktop/icon-sun.svg';
+        icon.setAttribute("alt", "sun icon");
+    }
+
+
+
+}
