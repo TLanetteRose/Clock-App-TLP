98ae25192d29178af3a197ff75c967d2f29084e4
T. Lanette <TLPollard4516@gmail.com>
2021-03-27 12:21:21 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+const background = document.getElementsByClassName('.background');
+const author = document.getElementsByClassName('.author');
+const icon = document.getElementsByClassName('.icon');
+const details = document.getElementsByClassName('.description'); 
+const period = document.getElementsByClassName('.period'); 
+const expand = document.getElementsByClassName('.expand');
+
