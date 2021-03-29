6ecd56102584dd6505501f5f2c77ca90171142e6
T. Lanette <TLPollard4516@gmail.com>
2021-03-29 11:43:56 -0400

    Update
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

---
+++ js/main2.js
@@ -0,0 +1,9 @@
+
+const URL_QUOTES = 'https://api.quotable.io/random';
+const URL_CLOCK = 'https://worldtimeapi.org/api/ip';
+
+const refresh = document.getElementById('refresh');
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+
+const time = document.getElementById('time');
